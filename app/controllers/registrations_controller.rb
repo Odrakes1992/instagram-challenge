2f0e5996ff020ee1b2dc6337183674ef97fe03b5
Osneil Drakes <osneil-drakes@hotmail.com>
2021-10-17 16:03:46 +0100

    update files directory

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,26 @@
+class RegistrationsController < ApplicationController
+ # instantiates new user
+  def new
+    @user = User.new
+  end
+  
+  def create
+    @user = User.new(user_params)
+    if @user.save
+    # stores saved user id in a session
+      session[:user_id] = @user.id
+      set_current_user
+      redirect_to posts_path, notice: 'Successfully created account'
+    else
+      render :new
+    end
+  end
+  
+  private
+  
+  def user_params
+    # strong parameters
+    params.require(:user).permit(:first_name,:email, :password, :password_confirmation)
+  end
+
+end
